90085a11b734b1ecb93c97f7a3cd595c0e0f63e3
kotoe <121812120+koto1996@users.noreply.github.com>
2023-03-03 16:45:55 +0000

    応用課題７/b

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -15,5 +16,7 @@ Rails.application.routes.draw do
   end
   get '/search',to: 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'chat/id',to: 'chats#show',as: 'chat'
+  resources :chats, only: [:create]
+  
 end
